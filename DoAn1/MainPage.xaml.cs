d8560b640d037fbb5d33d919ad0f8ef271940d41
TOÀN PHẠM MINH <toanphamhcmus@gmail.com>
2020-11-24 16:52:35 +0700

    add pj

---
+++ DoAn1/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DoAn1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        // Connection string for using Windows Authentication.
+        private string connectionString = @"Data Source=admin;Initial Catalog=MyStore;Integrated Security=True";
+
+        // This is an example connection string for using SQL Server Authentication.
+        // private string connectionString =
+        //     @"Data Source=YourServerName\YourInstanceName;Initial Catalog=DatabaseName; User Id=XXXXX; Password=XXXXX";
+
+        public string ConnectionString { get => connectionString; set => connectionString = value; }
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            DataTable data = null;
+            var products = new ObservableCollection<Product>();
+            
+            data = Provider.GetProducts(ConnectionString);
+            foreach (DataRow row in data.Rows)
+            {
+                var product = new Product();
+                product.Id = (int)row.ItemArray[0];
+                product.CatId = (int)row.ItemArray[1];
+                product.SKU = (string)row.ItemArray[2];
+                product.Name = (string)row.ItemArray[3];
+                product.Price = (Decimal)row.ItemArray[4];
+                product.Quantity = (int)row.ItemArray[5];
+                product.Description = (string)row.ItemArray[6];
+                product.Image = (string)row.ItemArray[7];
+
+                products.Add(product);
+            }
+
+
+            test_data.ItemsSource = products;
+        }
+
+
+
+    }
+}
---
+++ DoAn1/Provider/Provider.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAn1
+{
+    class Provider
+    {
+        string ConnectionString { get; set; }
+        Provider(string str)
+        {
+            ConnectionString = str;
+        }
+        SqlConnection Connection { get; set; }
+        public void Connect()
+        {
+            try
+            {
+                if (Connection == null)
+                    Connection = new SqlConnection(ConnectionString);
+                if (Connection != null && Connection.State != ConnectionState.Closed)
+                    Connection.Close();
+                Connection.Open();
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void DisConnect()
+        {
+            if (Connection != null && Connection.State == ConnectionState.Open)
+                Connection.Close();
+        }
+
+        public int ExcecuteNonQuery(CommandType cmtType, string sqlSQL,
+                        params SqlParameter[] parameters)
+        {
+            try
+            {
+                SqlCommand command = Connection.CreateCommand();
+                command.CommandText = sqlSQL;
+                command.CommandType = cmtType;
+                if (parameters != null && parameters.Length > 0)
+                    command.Parameters.AddRange(parameters);
+                int nRow = command.ExecuteNonQuery();
+                return nRow;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public DataTable ExcecuteQuery(CommandType cmtType, string strSQL,
+                    params SqlParameter[] parameters)
+        {
+            try
+            {
+                SqlCommand command = Connection.CreateCommand();
+                command.CommandType = cmtType;
+                command.CommandText = strSQL;
+                if (parameters != null && parameters.Length > 0)
+                    command.Parameters.AddRange(parameters);
+                SqlDataAdapter da = new SqlDataAdapter(command);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+        public static DataTable GetProducts(string ConnectionString)
+        {
+            //const string GetProductsQuery = "sp_GetProducts";
+            const string GetProductsQuery = "select * from Product join Category on Product.catid = Category.id";
+            DataTable dt = null;
+            Provider p = new Provider(ConnectionString);
+
+            var products = new ObservableCollection<Product>();
+            try
+            {
+                p.Connect();
+                dt = p.ExcecuteQuery(CommandType.Text, GetProductsQuery);
+                return dt;
+            }
+            catch (Exception eSql)
+            {
+                Debug.WriteLine("Exception: " + eSql.Message);
+            }
+            finally
+            {
+                p.DisConnect();
+            }
+            return null;
+        }
+    }
+}
